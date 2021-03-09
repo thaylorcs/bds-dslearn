de929d6ad35ebf3b8ee0bff65184babbf026fbb2
Thaylor Carvalho <thaylorcs@outlook.com>
2021-03-09 17:25:53 -0300

    Fix: missing associations, getters

--- backend/src/main/java/com/devsuperior/dslearnbds/entities/Lesson.java
+++ backend/src/main/java/com/devsuperior/dslearnbds/entities/Lesson.java
@@ -1,11 +1,12 @@
 package com.devsuperior.dslearnbds.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -15,6 +16,7 @@ import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 @Entity
@@ -33,6 +35,9 @@ public abstract class Lesson implements Serializable {
 	@ManyToOne
 	@JoinColumn(name = "section_id")
 	private Section section;
+	
+	@OneToMany(mappedBy = "lesson")
+	private List<Deliver> deliveries = new ArrayList<>();
 	
 	@ManyToMany
 	@JoinTable(name = "tb_lessons_done",
@@ -88,6 +93,10 @@ public abstract class Lesson implements Serializable {
 
 	public Set<Enrollment> getEnrollmentsDone() {
 		return enrollmentsDone;
+	}
+
+	public List<Deliver> getDeliveries() {
+		return deliveries;
 	}
 
 	@Override
--- backend/src/main/java/com/devsuperior/dslearnbds/entities/User.java
+++ backend/src/main/java/com/devsuperior/dslearnbds/entities/User.java
@@ -85,9 +85,9 @@ public class User implements Serializable{
 	public Set<Role> getRoles() {
 		return roles;
 	}
-
-	public void setRoles(Set<Role> roles) {
-		this.roles = roles;
+	
+	public List<Notification> getNotifications() {
+		return notifications;
 	}
 
 	@Override
